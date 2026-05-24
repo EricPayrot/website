55f19488d559978b976655a0ab59b83608ec49ca
Eric Payrot <eric.payrot@free.fr>
2026-05-24 16:30:56 +0200

    contact form and thank you page

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,15 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { siteOrigin } from "./src/config/site";
 
+const thanksPath = `${siteOrigin.replace(/\/$/, "")}/contact/thanks`;
+
 export default defineConfig({
   site: siteOrigin,
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => page.replace(/\/$/, "") !== thanksPath,
+    }),
+  ],
   prefetch: {
     prefetchAll: true,
   },
