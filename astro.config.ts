ac4f4f1a76f00ca269dae889b35b45e779caa113
Eric Payrot <eric.payrot@free.fr>
2026-06-12 15:31:55 +0200

    bug fix

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import { siteOrigin } from "./src/config/site";
 
 export default defineConfig({
+  output: "static",
   site: siteOrigin,
   integrations: [sitemap()],
   prefetch: {
